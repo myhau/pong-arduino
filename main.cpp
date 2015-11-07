5ecf0d899c13f9fedf129fab2b67f351a040e6c6
myhau <mmyhau@gmail.com>
2015-11-07 23:16:56 +0100

    works mocked

--- main.cpp
+++ main.cpp
@@ -4,8 +4,9 @@
 #include <iostream>
 #include <ctime>
 #include <sys/time.h>
+#include <cmath>
 
-typedef int uint8_t;
+//typedef int uint8_t;
 
 using namespace sf;
 
@@ -34,28 +35,27 @@ int player2Score = 0;
 
 int start = 1;
 
-virtual class InputDevice {
+class InputDevice {
 public:
-//    InputDevice() {}
-    virtual uint8_t getAngle()  = 0;
+    virtual int getAngle()  = 0;
 };
 
-class JoystickInputDevice: InputDevice {
+class JoystickInputDevice: public InputDevice {
 
 };
 
-class MockInputDevice: InputDevice {
+class MockInputDevice: public InputDevice {
 private:
-    uint8_t angle;
+    int angle;
 
 public:
     MockInputDevice(): angle(0) {
 
     }
-    uint8_t getAngle() override {
-
+    int getAngle() override {
+        return angle;
     }
-    void setAngle(uint8_t angle) {
+    void setAngle(int angle) {
         this->angle = angle;
     }
 };
@@ -64,14 +64,45 @@ class BluetoothInputDevice: InputDevice {
 
 };
 
-
-virtual class Renderer {
+class Renderer {
 public:
     virtual void drawRect(uint8_t x, uint8_t y, uint8_t width, uint8_t height, Color c, bool filled = false) = 0;
     virtual void drawCircle(uint8_t x, uint8_t y, uint8_t radius, Color c, bool filled = false) = 0;
     virtual void clearScreen(Color c) = 0;
     virtual uint8_t screenWidth() = 0;
-    virtual uint8_t screenHeight() = 0
+    virtual uint8_t screenHeight() = 0;
+};
+
+class GameObject {
+    virtual void update() = 0;
+    virtual void render(Renderer* r) = 0;
+};
+
+class Board: GameObject {
+private:
+    uint8_t w;
+    uint8_t h;
+    uint8_t thickness;
+public:
+    Board(uint8_t width, uint8_t height, uint8_t thickness):
+            w(width), h(height), thickness(thickness) {
+    }
+
+    uint8_t getWidth() const {
+        return w;
+    }
+
+    uint8_t getHeight() const {
+        return h;
+    }
+
+    void update() {
+    }
+
+    void render(Renderer* r) {
+        r->drawRect(0, 0, w, h, Color::White);
+    }
+
 };
 
 class DesktopRenderer: public Renderer {
@@ -99,7 +130,6 @@ public:
     }
 
     void check_and_clear() {
-        std::cout << "HEHE" << std::endl;
         if(window.isOpen()) {
 
             Event event;
@@ -109,23 +139,23 @@ public:
                     window.close();
                 if (event.type == Event::KeyPressed) {
                     const int n = 9;
-                    const int max_joy_state = 1024;
+                    const int joy_states = 1024;
+                    const int min_joy_state = -512;
+                    const int max_joy_state = 512;
                     Keyboard::Key k[] =
                             {Keyboard::A, Keyboard::S, Keyboard::D, Keyboard::F,
                              Keyboard::G, Keyboard::H, Keyboard::J, Keyboard::K, Keyboard::L};
-                    uint8_t joystick_state = 0;
+                    int joystick_state = 0;
                     for (int i = 0; i < n; i++) {
                         if (k[i] == event.key.code) {
-                            joystick_state = (uint8_t) ((double) (i + 1) * ((double) max_joy_state / (double) n));
+                            joystick_state = (int) ((double) (i + 1) * ((double) joy_states / (double) n));
                         }
                     }
+                    joystick_state += min_joy_state;
                     mockInputDevice.setAngle(joystick_state);
 
                 }
             }
-
-            // clear the window with black color
-//            window.clear(sf::Color::Black);
         }
     }
 
@@ -133,165 +163,51 @@ public:
         if(window.isOpen()) window.display();
     }
 
-    void drawRect(uint8_t x0, uint8_t y0, uint8_t w1, uint8_t h1, char c1) {
-        drawRect(x0, y0, w1, h1, c1, 123);
-    }
-
-
-    void drawRect(uint8_t x0, uint8_t y0, uint8_t w1, uint8_t h1, char c1, char invert) {
+    void drawRect(uint8_t x0, uint8_t y0, uint8_t w1, uint8_t h1, Color c, bool filled = false) override {
         check_and_clear();
         RectangleShape rs;
         rs.setSize(Vector2f(w1, h1));
         rs.setPosition(x0, y0);
-        if(invert == INVERT) {
-            rs.setFillColor(Color::White);
-            rs.setOutlineColor(Color::Black);
+        rs.setOutlineColor(c);
+        if(filled) {
+            rs.setFillColor(c);
         }
-        if(invert != INVERT) {
-            rs.setFillColor(Color::Black);
-            rs.setOutlineColor(Color::White);
-        }
-
+        else rs.setFillColor(Color(Color(255, 255, 255).toInteger() - c.toInteger()));
         rs.setOutlineThickness(1);
-
         window.draw(rs);
         draw_window();
     }
-    void drawCircle(uint8_t x, uint8_t y, uint8_t r, char c1, char c2) {
+
+    void drawCircle(uint8_t x, uint8_t y, uint8_t r, Color c, bool filled = false) override {
         check_and_clear();
         CircleShape cs(r, 10);
         cs.setPosition(x, y);
-        cs.setFillColor(Color::White);
+        if(filled)
+            cs.setFillColor(c);
+        cs.setOutlineThickness(1);
         window.draw(cs);
         draw_window();
     };
-    void clearScreen() {
+    void clearScreen(Color c) override {
         check_and_clear();
-//        window./
         RectangleShape clearRect;
         clearRect.setSize(Vector2f(this->w, this->h));
-        std::cout << this->w << std::endl;
         clearRect.setPosition(0, 0);
-        clearRect.setFillColor(sf::Color::Black);
+        clearRect.setFillColor(c);
         window.draw(clearRect);
         draw_window();
     };
 
-    void draw_it() {
-        draw_window();
-    }
-
-    void delay_frame(uint8_t x) {
-
-    }
 };
 
 class ArduinoRenderer: Renderer {
 
 };
 
-virtual class GameObject {
-    virtual void update() = 0;
-    virtual void render(Renderer* r) = 0;
-};
-
-class Ball: GameObject {
-
-private:
-    uint8_t radius;
-    int8_t velocity_x;
-    int8_t velocity_y;
-    uint8_t pos_x;
-    uint8_t pos_y;
-    Board* board;
-    Paddle* paddle1;
-    Paddle* paddle2;
-
-
-    bool didCollide(Paddle* p) {
-        bool isLeftPaddle = p->getWhichPaddle() == Paddle::left;
-        bool collided = false;
-        collided |=
-                (isLeftPaddle && p->getPosX() + p->getWidth() >= pos_x - radius)
-                | (isLeftPaddle && p->getPosX() - p->getWidth() >= pos_x + radius);
-        collided |= pos_x > p->getPosY() && (p->getPosY() + p->getHeight()) > pos_x;
-        return collided;
-    }
-
-    bool didCollide(Board* b) {
-        bool collided = false;
-        collided |= pos_x - radius <= 0;
-        collided |= pos_x + radius >= b->getWidth();
-        collided |= pos_y - radius <= 0;
-        collided |= pos_y + radius >= b->getHeight();
-        return collided;
-    }
-
-    void handleCollision(Paddle* p) {
-        velocity_x = -velocity_x;
-        velocity_y += p->getVelocityY(); // TODO: change :D
-    }
-    void handleCollision(Board* b) {
-        if(pos_x - radius <= 0 | pos_y + radius >= b->getHeight()) velocity_x = -velocity_x;
-        if(pos_y - radius <= 0 | pos_x + radius >= b->getWidth()) velocity_y = -velocity_y;
-    }
-
-
-public:
-    Ball(uint8_t radius, Board* b, Paddle* paddle1, Paddle* paddle2):
-            board(b), paddle1(paddle1), paddle2(paddle2),
-            radius(radius), velocity_x(0), velocity_y(0), pos_x(0), pos_y(0) {
-    }
-    Ball(uint8_t radius, uint8_t initial_pos_x, uint8_t initial_pos_y, Board* b, Paddle* paddle1, Paddle* paddle2):
-            Ball(radius, b, paddle1, paddle2), pos_x(initial_pos_x), pos_y(initial_pos_y) {}
-
-    void update() {
-        if(didCollide(paddle1))
-            handleCollision(paddle1);
-        else if(didCollide(paddle2))
-            handleCollision(paddle2);
-        else if(didCollide(board))
-            handleCollision(board);
-
-        pos_x += velocity_x;
-        pos_y += velocity_y;
-
-    }
-    void render(Renderer* r) {
-        r->drawCircle(pos_x, pos_y, radius, Color::White, true);
-    }
-
-};
-
-class Board: GameObject {
-private:
-    uint8_t w;
-    uint8_t h;
-    uint8_t thickness;
-public:
-    Board(uint8_t width, uint8_t height, uint8_t thickness):
-            w(width), h(height), thickness(thickness) {
-    }
-
-
-    uint8_t getWidth() const {
-        return w;
-    }
-
-    uint8_t getHeight() const {
-        return h;
-    }
-
-    void update() {
-    }
-
-    void render(Renderer* r) {
-        r->drawRect(0, 0, w, h, Color::White);
-    }
-
-};
 
 class Paddle: GameObject {
+public:
+    typedef char Which;
 private:
     Which which_paddle;
     uint8_t w;
@@ -302,9 +218,8 @@ private:
     Board* board;
     InputDevice* device;
 public:
-    typedef char Which;
-    static Which left = 0;
-    static Which right = 1;
+    static const Which left = 0;
+    static const Which right = 1;
 
     Paddle(Paddle::Which which, uint8_t paddle_width, uint8_t paddle_height, Board* b, InputDevice* device1):
             device(device1), board(b), which_paddle(which), w(paddle_width), h(paddle_height), velocity_y(0), pos_y(0) {
@@ -348,13 +263,93 @@ public:
     }
 };
 
-struct GameConfig {
-    uint8_t ballRadius = 5;
-    uint8_t paddleHeight = 5;
-    uint8_t paddleWidth = 50;
-    uint8_t boardWidth = 120;
-    uint8_t boardHeight = 96;
-    uint8_t frameRate = 30;
+class Ball: GameObject {
+
+private:
+    uint8_t radius;
+    int8_t velocity_x;
+    int8_t velocity_y;
+    uint8_t pos_x;
+    uint8_t pos_y;
+    Board* board;
+    Paddle* paddle1;
+    Paddle* paddle2;
+
+    bool didCollide(Paddle* p) {
+        bool isLeftPaddle = p->getWhichPaddle() == Paddle::left;
+        bool collided = false;
+        collided |=
+                (isLeftPaddle && p->getPosX() + p->getWidth() >= pos_x - radius)
+                | (isLeftPaddle && p->getPosX() - p->getWidth() >= pos_x + radius);
+        collided |= pos_x > p->getPosY() && (p->getPosY() + p->getHeight()) > pos_x;
+        return collided;
+    }
+
+    bool didCollide(Board* b) {
+        bool collided = false;
+        collided |= pos_x - radius <= 0;
+        collided |= pos_x + radius >= b->getWidth();
+        collided |= pos_y - radius <= 0;
+        collided |= pos_y + radius >= b->getHeight();
+        return collided;
+    }
+
+    void handleCollision(Paddle* p) {
+        velocity_x = -velocity_x;
+        velocity_y += p->getVelocityY(); // TODO: change :D
+    }
+    void handleCollision(Board* b) {
+        if(pos_x - radius <= 0 | pos_y + radius >= b->getHeight()) velocity_x = -velocity_x;
+        if(pos_y - radius <= 0 | pos_x + radius >= b->getWidth()) velocity_y = -velocity_y;
+    }
+
+
+public:
+    Ball(uint8_t radius, Board* b, Paddle* paddle1, Paddle* paddle2):
+            board(b), paddle1(paddle1), paddle2(paddle2),
+            radius(radius), velocity_x(0), velocity_y(0), pos_x(b->getWidth()/2), pos_y(b->getHeight()/2) {
+    }
+    Ball(uint8_t radius, uint8_t initial_pos_x, uint8_t initial_pos_y, Board* b, Paddle* paddle1, Paddle* paddle2):
+            Ball(radius, b, paddle1, paddle2) { pos_x = initial_pos_x; pos_y = initial_pos_y; }
+
+    void update() {
+        if(didCollide(paddle1))
+            handleCollision(paddle1);
+        else if(didCollide(paddle2))
+            handleCollision(paddle2);
+        else if(didCollide(board))
+            handleCollision(board);
+
+        pos_x += velocity_x;
+        pos_y += velocity_y;
+
+    }
+    void render(Renderer* r) {
+        r->drawCircle(pos_x, pos_y, radius, Color::White, true);
+    }
+
+};
+
+
+
+
+class GameConfig {
+
+public:
+    uint8_t ballRadius;
+    uint8_t paddleHeight;
+    uint8_t paddleWidth;
+    uint8_t boardWidth;
+    uint8_t boardHeight;
+    uint8_t frameRate;
+    GameConfig():
+            ballRadius(5),
+            paddleHeight(50),
+            paddleWidth(5),
+            boardWidth(240),
+            boardHeight(192),
+            frameRate(30)
+    {}
 };
 
 class FrameRateHandler {
@@ -379,7 +374,18 @@ public:
         lastUpdate = nowInUs();
     }
     bool shouldUpdateFrame() {
-        return nowInUs() - lastUpdate > shouldUpdateEvery;
+        std::cout << shouldUpdateEvery << std::endl;
+        std::cout << lastUpdate << std::endl;
+
+        long timeLeft = shouldUpdateEvery - (nowInUs() - lastUpdate);
+
+        std::cout << timeLeft << std::endl;
+
+        if(timeLeft > 0) {
+            timeLeft = std::min(timeLeft, shouldUpdateEvery);
+            sf::sleep(sf::microseconds(timeLeft));
+        }
+        return true;
     }
 };
 
@@ -392,10 +398,11 @@ private:
     Renderer* renderer;
     FrameRateHandler frh;
 public:
-    Game(Renderer* renderer, GameConfig* gameConfig, InputDevice* inputDevice):     
+    Game(Renderer* renderer, GameConfig* gameConfig, InputDevice* inputDevice1, InputDevice* inputDevice2):
+            renderer(renderer),
             board(gameConfig->boardWidth, gameConfig->boardHeight, 1),
-            paddle1(Paddle::left, gameConfig->paddleWidth, gameConfig->paddleHeight, &board, inputDevice),
-            paddle2(Paddle::right, gameConfig->paddleWidth, gameConfig->paddleHeight, &board, inputDevice),
+            paddle1(Paddle::left, gameConfig->paddleWidth, gameConfig->paddleHeight, &board, inputDevice1),
+            paddle2(Paddle::right, gameConfig->paddleWidth, gameConfig->paddleHeight, &board, inputDevice2),
             ball(gameConfig->ballRadius, &board, &paddle1, &paddle2),
             frh(gameConfig->frameRate)
     {
@@ -407,11 +414,13 @@ public:
 
         renderer->clearScreen(Color::Black);
 
+        std::cout << "HEHE" << std::endl;
+
         board.update(); board.render(renderer);
         paddle1.update();
         paddle2.update();
         ball.update();
-        
+
         paddle1.render(renderer);
         paddle2.render(renderer);
         ball.render(renderer);
@@ -542,11 +551,14 @@ public:
 Game* game;
 GameConfig gameConfig;
 DesktopRenderer* desktopRenderer;
-
+MockInputDevice* iDevice2;
 void setup() {
     desktopRenderer = new DesktopRenderer(gameConfig.boardWidth, gameConfig.boardHeight);
     InputDevice* iDevice = desktopRenderer->getInputDevice();
-    game = new Game(desktopRenderer, &gameConfig, iDevice);
+    iDevice2 = new MockInputDevice;
+    iDevice2->setAngle(0);
+
+    game = new Game(desktopRenderer, &gameConfig, iDevice, iDevice2);
     game->start();
 }
 
